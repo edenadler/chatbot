e338b704e1e21c450199d6e10dc5ec63ea271c36
edenadler <eden.n.adler@gmail.com>
2017-01-26 19:16:29 +0200

    organized general structure and began processing user input

--- boto.py
+++ boto.py
@@ -1,8 +1,10 @@
 """
 This is the template server side for ChatBot
 """
+from datetime import datetime
 from bottle import route, run, template, static_file, request
 import json
+
 
 @route('/', method='GET')
 def index():
@@ -11,19 +13,64 @@ def index():
 
 @route("/chat", method='POST')
 def chat():
+    user_message = request.POST.get('msg')
+    return_message = split_message(user_message)
+    return json.dumps(boto_response(return_message))
+
+def boto_response(return_message):
+    return {"animation": return_message[1], "msg": return_message[0]}
+
+def split_message(user_message):
+    user_message = user_message.lower()
+    words = user_message.lower().split()
+    return interpret_words(words, user_message)
+
+def interpret_words(words, user_message):
     GREETING_WORDS = ["hello", "hi", "hey", "yo", "heya", "shalom"]
     QUESTION_WORDS = ["who","what","where","when","why","how"]
-    user_message = request.POST.get('msg')
-    return_message = "I'm sorry, I don't understand"
-    words = user_message.split()
     for word in words:
-        if word.lower() in GREETING_WORDS:
-            return_message = "Hi, how can I help you?"
-            return json.dumps({"animation": "excited", "msg": return_message})
-        elif word.lower() in QUESTION_WORDS or word[-1] == "?":
-            return_message = "That's a great question."
-            return json.dumps({"animation": "confused", "msg": return_message})
-    return json.dumps({"animation": "confused", "msg": return_message})
+        if word in GREETING_WORDS:
+            return greeting(words, user_message)
+        elif word in QUESTION_WORDS or user_message.endswith("?") or "calculate" in user_message:
+            return question(words, user_message)
+        else:
+            return "I'm sorry, I don't understand", "confused"
+
+def greeting(words, user_message):
+    variations = ["im","i'm","name is"]
+    for word in variations:
+        if word in user_message:
+            name = " "+user_message.split(word + " ",1)[1].capitalize() +", "
+        else:
+            name = ""
+    return "Hi,"+name+"how can I help you?", "waiting"
+
+
+def question(words, user_message):
+    math_operations = ["+","-","/","*","plus","minus","divided by","multiplied by"]
+    if "calculate" in user_message:
+        for operator in math_operations:
+            if operator in user_message:
+                # index = user_message.index(operator)
+                result = eval(user_message[10:])
+                print(result)
+                return "The answer is: "+str(result), "dancing"
+            else:
+                return "I don't know how to calculate that, I'm sorry", "crying"
+
+    if "what" in user_message:
+        print("what")
+        if "time" in user_message:
+            #TODO:get rid of miliseconds
+            continuation = "the time is "+str(datetime.time(datetime.now()))
+        elif any(operator in math_operations for operator in user_message):
+            continuation = 'if you want me to calculate it for you, say: "Calculate" followed by the thing you want me to calculate. For example: Calculate 4+4'
+        else:
+            return "I'm sorry, I don't understand", "confused"
+    else:
+        return "I'm sorry, I don't understand", "confused"
+
+    return "Great question, "+continuation, "ok"
 
 
 @route("/test", method='POST')
